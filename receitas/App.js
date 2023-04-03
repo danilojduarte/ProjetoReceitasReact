d7437369538bf553e355014d5d85b6a279582214
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-03 15:57:58 -0300

    Rotas geradas com sucesso

--- receitas/App.js
+++ receitas/App.js
@@ -1,28 +1,13 @@
-import {View,Text, StyleSheet} from 'react-native'
+import { View, Text } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { Routes } from './src/routes'
+
 
 export default function App(){
   return(
-    <View  style={styles.container}>
-      <Text style={styles.title}>Meu primeiro App</Text>
-      <Nome />
-      <Nome />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container:{
-    
-    height:400
-  },
-  title:{
-    fontSize: 24,
-    marginTop: 100
-  }
-})
-
-function Nome() {
-  return(
-    <Text>Olá Danilo!</Text>
+    <NavigationContainer>
+    <Routes/>
+    </NavigationContainer>
   )
 }
