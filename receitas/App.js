7f64286388cc5fe161d9aa06337d12c360e1b9f8
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-03 15:04:30 -0300

    Adicionadas novas bibliotecas

--- receitas/App.js
+++ receitas/App.js
@@ -2,15 +2,27 @@ import {View,Text, StyleSheet} from 'react-native'
 
 export default function App(){
   return(
-    <View>
-      <Text>Meu primeiro App</Text>
+    <View  style={styles.container}>
+      <Text style={styles.title}>Meu primeiro App</Text>
+      <Nome />
+      <Nome />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: "red",
+    
     height:400
+  },
+  title:{
+    fontSize: 24,
+    marginTop: 100
   }
 })
+
+function Nome() {
+  return(
+    <Text>Olá Danilo!</Text>
+  )
+}
