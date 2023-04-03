8e4d2c647c0827759faae9ade3ad5ebd8cc138c2
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-03 14:42:50 -0300

    Criando primeiro componente

--- receitas/App.js
+++ receitas/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text, StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View>
+      <Text>Meu primeiro App</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    backgroundColor: "red",
+    height:400
+  }
+})
