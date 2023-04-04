379a564606d3d6c01827e30bc416deb41976f520
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-04 14:06:17 -0300

    Tela de navegação estilizada

--- receitas/src/routes/index.js
+++ receitas/src/routes/index.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { Home } from '../pages/home'
 import { Favorites } from '../pages/favorites'
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 
@@ -21,18 +22,30 @@ export function Routes() {
             }}
         >
             
-
-
             <Tab.Screen 
                 name="HomeTab" 
                 component={Home}
                 options={{
-                    
+                    tabBarIcon: ({ color, size, focused }) => {
+                        if(focused) {
+                            return <Ionicons name="home" color="#000" size={size} />
+                        }
+
+                            return <Ionicons name="home-outline" color={color} size={size} />
+                    }
                 }}
             />
             <Tab.Screen 
                 name="Favorites" 
-                component={Favorites} 
+                component={Favorites}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => {
+                        if(focused){
+                            return <Ionicons name="heart" color="#ff4141" size={size} />
+                        }
+                        return <Ionicons name="heart-outline" color={color} size={size} />
+                    }
+                }}
             />
         </Tab.Navigator>
     )
