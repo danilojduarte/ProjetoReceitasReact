7f8203c2967521c35f1aa23c28298858f1c9a00f
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-04 15:09:35 -0300

    Finalizado botão de search

--- receitas/src/pages/home/index.js
+++ receitas/src/pages/home/index.js
@@ -1,12 +1,35 @@
 
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import { useState  } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 
+import { Ionicons } from '@expo/vector-icons'
 import { Logo } from '../../components/logo'
 
 export function Home(){
+    const [inputValue, setInputValue] = useState("")
+
+    function handleSearch() {
+        console.log("Você: ")
+        console.log(inputValue)
+    }
+
     return (
     <SafeAreaView style={styles.container}>
         <Logo />
+        <Text style={styles.title}>Econtre a receita</Text>
+        <Text style={styles.title}>Que combina com você</Text>
+
+        <View style={styles.form}>
+        <TextInput 
+            placeholder='Digite o nome da comida...'
+            style={styles.input}
+            value={inputValue}
+            onChangeText={ (text) => setInputValue(text) }
+        />
+        <TouchableOpacity onPress={handleSearch} >
+            <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+        </View>
     </SafeAreaView>
     )
 }
@@ -19,5 +42,29 @@ const styles = StyleSheet.create({
         paddingTop: 36,
         paddingStart: 14,
         paddingEnd: 14,
+    },
+    title:{
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: "#0e0e0e"
+    },
+    form:{
+        backgroundColor: '#fff',
+        width:'100%',
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor:'#ececec',
+        paddingLeft: 8,
+        paddingRight: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    input:{
+        width: '90%',
+        maxWidth: '90%',
+        height: 54
     }
 })
