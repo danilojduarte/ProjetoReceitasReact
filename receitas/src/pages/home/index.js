92d1af00bf20bfc279905bb8bfde02ad498ee5cd
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-04 14:24:02 -0300

    Logo concluida

--- receitas/src/pages/home/index.js
+++ receitas/src/pages/home/index.js
@@ -1,17 +1,23 @@
 
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+
+import { Logo } from '../../components/logo'
 
 export function Home(){
     return (
-    <View style={styles.container}>
-        <Text>Página Home!!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <Logo />
+    </SafeAreaView>
     )
 }
 
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'red',
+        flex:1,
+        backgroundColor: '#f3f9ff',
+        paddingTop: 36,
+        paddingStart: 14,
+        paddingEnd: 14,
     }
 })
