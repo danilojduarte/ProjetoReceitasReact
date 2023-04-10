8409646caa22d58d6a0d41a960c689a342f42d62
DaniloDuarte <danilojoseduarte@gmail.com>
2023-04-10 14:25:28 -0300

    Atribuindo API

---
+++ receitas/src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://http://192.168.2.14:3000/'
+})
+
+
+
+
+
+
+/*
+Como Rodar com IPV4: json-server --watch -d -180 --host (meu ip) 192.168.0.12 db.jason
+*/
